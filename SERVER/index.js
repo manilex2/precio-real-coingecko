6d52477edb32dd622c775285db79b1fa594a4647
Manuel Pereira <manilex2@gmail.com>
2022-02-24 03:08:15 -0500

    Terminada la funcion y configurada para AWS

--- SERVER/index.js
+++ SERVER/index.js
@@ -17,12 +17,64 @@ app.get('/', async (req, res) => {
         database: database.database,
         port: database.port
     });
-    var sql = `SELECT name FROM ${process.env.TABLE_CRIPTOS}`;
-    conexion.query(sql, async function (err, resultado) {
-        if (err) throw err;
-        console.log('Conexion establecida con la base de datos');
-        console.log(resultado);
-    });
+    try {
+        let sql = `SELECT name FROM ${process.env.TABLE_LIST_CRIPTOS}`;
+        conexion.query(sql, async function (err, resultado) {
+            if (err) throw err;
+            console.log('Conexion establecida con la base de datos');
+            await agregarPrecios(resultado);
+        });
+    } catch (error) {
+        console.error(error)
+    }
+    async function agregarPrecios(resultado) {
+        var cripto = [];
+        for (let i = 0; i < resultado.length; i++) {
+            cripto.push(resultado[i].name);
+        }
+        const CRIPTOSJOIN = cripto.join(',');
+        await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${CRIPTOSJOIN}&vs_currencies=usd`)
+        .then((res) => {
+            return res.json();
+        })
+        .then((json) => {
+            var respuesta = json;
+            evaluarDatos(respuesta);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+    async function evaluarDatos(respuesta) {
+        var datos = [];
+        var fecha = new Date();
+        var mes = fecha.getMonth() + 1;
+        fechaFormateada = `${fecha.getFullYear()}-${mes}-${fecha.getDate()}`;
+        for (const coin in respuesta) {
+            for (const precio in respuesta[coin]) {
+                datos.push([coin, fechaFormateada, respuesta[coin][precio]]);
+            };
+        };
+        let sql = `DELETE FROM ${process.env.TABLE_PRICE_CRIPTOS} WHERE fecha = '${fechaFormateada}'`;
+        conexion.query(sql, async (err) => {
+            if (err) throw err;
+        });
+        let sql2 = `ALTER TABLE ${process.env.TABLE_PRICE_CRIPTOS} AUTO_INCREMENT = 1`;
+        conexion.query(sql2, async (err) => {
+            if (err) throw err;
+        });
+        let sql3 = `INSERT INTO ${process.env.TABLE_PRICE_CRIPTOS} (name, fecha, precio) VALUES ?`;
+        conexion.query(sql3, [datos], async (err, resultado) => {
+            if (err) throw err;
+            console.log(resultado);
+            await finalizarEjecucion();
+        });
+        console.log(sql, sql2, sql3)
+    }
+    async function finalizarEjecucion() {
+        conexion.end();
+        res.send('Ejecutado');
+    }
 });
 
 app.listen(process.env.PORT || PUERTO, () => {
